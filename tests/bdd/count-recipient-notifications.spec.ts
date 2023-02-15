ab610c02366dd32c82fe2a7f136a4a038ad12946
Yury Stolarz <yurysscareer@gmail.com>
2023-02-15 19:16:01 -0300

    test: add behavior test to count recipient notification use case

---
+++ tests/bdd/count-recipient-notifications.spec.ts
@@ -0,0 +1,22 @@
+import * as chai from 'chai';
+import { CountRecipientNotifications } from '../../src/domain/use-cases';
+import InMemoryNotificationsRepository from '../repository/InMemoryNotificationsRepository';
+import makeNotification from '../factory/notification-factory';
+
+const { expect } = chai;
+
+describe('Count notifications by recipient id', function () {
+  const notificationRepository = new InMemoryNotificationsRepository();
+  const countAllRecipientNotifications = new CountRecipientNotifications(notificationRepository);
+
+  it('Should count the number of notifications from a recipient', async function () {
+    const recipientId = 10;
+    await notificationRepository.create(makeNotification(recipientId));
+    await notificationRepository.create(makeNotification(recipientId));
+    await notificationRepository.create(makeNotification(recipientId));
+
+    const result = await countAllRecipientNotifications.execute({ recipientId });
+
+    expect(result).to.equal(3);
+  });
+});
