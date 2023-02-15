d617a3ae785df94e23e75581e9c22c4eb94c08b8
Yury Stolarz <yurysscareer@gmail.com>
2023-02-15 19:07:44 -0300

    test: add methods findById and save in database memory

--- tests/repository/InMemoryNotificationsRepository.ts
+++ tests/repository/InMemoryNotificationsRepository.ts
@@ -7,4 +7,16 @@ export default class InMemoryNotificationsRepository implements NotificationRepo
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
+
+  async findById(notificationId: number) {
+    const notificaiton = this.notifications.find(({ id }) => id === notificationId);
+
+    return notificaiton;
+  }
+
+  async save(notification: Notification) {
+    const notificationIndex = this.notifications.findIndex(({ id }) => id === notification.id);
+
+    this.notifications[notificationIndex] = notification;
+  }
 }
