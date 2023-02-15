116c7dd3e7d3cf6cae607ce564d65d56d101177c
Yury Stolarz <yurysscareer@gmail.com>
2023-02-15 19:08:16 -0300

    test: add test of cancel notification

---
+++ tests/bdd/cancel-notification.spec.ts
@@ -0,0 +1,38 @@
+import * as chai from 'chai';
+import { CancelNotification } from '../../src/domain/use-cases';
+import InMemoryNotificationsRepository from '../repository/InMemoryNotificationsRepository';
+import makeNotification from '../factory/notification-factory';
+import CustomError from '../../src/domain/error/CustomError';
+
+const { expect } = chai;
+
+describe('Cancel notification', function () {
+  const notificationRepository = new InMemoryNotificationsRepository();
+  const cancelNotification = new CancelNotification(notificationRepository);
+
+  it('Should be able to cancel notificaiton', async function () {
+    const recipientId = 10;
+    const newNotifiaction = makeNotification(recipientId);
+
+    await notificationRepository.create(newNotifiaction);
+
+    await cancelNotification.execute({ notificationId: newNotifiaction.id });
+
+    expect(notificationRepository.notifications[0].canceledAt).to.instanceOf(Date);
+  });
+
+  it('Should throw an error when not finding a notification', async function () {
+    const recipientId = 10;
+    const newNotifiaction = makeNotification(recipientId);
+
+    await notificationRepository.create(newNotifiaction);
+
+    try {
+      const invalidId = 9999;
+      await cancelNotification.execute({ notificationId: invalidId });
+    } catch (error) {
+      expect((error as CustomError).name).to.equal('Not Found');
+      expect((error as CustomError).message).to.equal('Notification Not Found');
+    }
+  });
+});
