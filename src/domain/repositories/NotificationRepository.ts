980234a0ba25009126bb16303de0725bf6592038
Yury Stolarz <yurysscareer@gmail.com>
2023-02-15 19:06:23 -0300

    feat: add in notification repository the methods findById and save

--- src/domain/repositories/NotificationRepository.ts
+++ src/domain/repositories/NotificationRepository.ts
@@ -2,4 +2,6 @@ import Notification from '../entities/Notification';
 
 export default abstract class NotificationRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: number): Promise<Notification | undefined>;
+  abstract save(notification: Notification): Promise<void>;
 }
