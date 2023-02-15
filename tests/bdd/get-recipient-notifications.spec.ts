e4dda07b28cbae3830f06de9e228b89901e4ddf9
Yury Stolarz <yurysscareer@gmail.com>
2023-02-15 19:24:32 -0300

    test: add test of count recipient notifications

---
+++ tests/bdd/get-recipient-notifications.spec.ts
@@ -0,0 +1,26 @@
+import * as chai from 'chai';
+import { GetRecipientNotifications } from '../../src/domain/use-cases';
+import InMemoryNotificationsRepository from '../repository/InMemoryNotificationsRepository';
+import makeNotification from '../factory/notification-factory';
+
+const { expect } = chai;
+
+describe('Get All notifications by recipient id', function () {
+  const notificationRepository = new InMemoryNotificationsRepository();
+  const getAllRecipientNotifications = new GetRecipientNotifications(notificationRepository);
+
+  it('Should return all notifications from a recipient', async function () {
+    const recipientId = 10;
+    await notificationRepository.create(makeNotification(recipientId));
+    await notificationRepository.create(
+      makeNotification(recipientId, 'Você foi aceito no processo seletivo', 'profissional'),
+    );
+
+    const getNotificationsByRecipientId = await notificationRepository
+      .findManyByRecipientId(recipientId);
+
+    const notifications = await getAllRecipientNotifications.execute({ recipientId });
+
+    expect(notifications).to.deep.equal(getNotificationsByRecipientId);
+  });
+});
