f9bb3fd486463aaf6f32f999ca0a9ee78bb36a57
Yury Stolarz <yurysscareer@gmail.com>
2023-02-15 18:58:10 -0300

    test: add test of use case send notification

---
+++ tests/bdd/send-notifications.spec.ts
@@ -0,0 +1,57 @@
+import * as chai from 'chai';
+import CustomError from '../../src/domain/error/CustomError';
+import { SendNotification } from '../../src/domain/use-cases';
+import InMemoryNotificationsRepository from '../repository/InMemoryNotificationsRepository';
+
+const { expect } = chai;
+
+describe('Send notification', function () {
+  it('Should be able to send notificaiton', async function () {
+    const notificationRepository = new InMemoryNotificationsRepository();
+
+    const sendNotification = new SendNotification(notificationRepository);
+
+    const { notification } = await sendNotification.execute({
+      recipientId: 10,
+      category: 'social',
+      content: 'This is a notification',
+    });
+
+    expect(notificationRepository.notifications).to.have.length(1);
+    expect(notificationRepository.notifications[0]).to.deep.equal(notification);
+  });
+
+  it('Should return an error when the content size is wrong', async function () {
+    const notificationRepository = new InMemoryNotificationsRepository();
+
+    const sendNotification = new SendNotification(notificationRepository);
+
+    try {
+      await sendNotification.execute({
+        recipientId: 10,
+        category: 'social',
+        content: 'erro',
+      });
+    } catch (error) {
+      expect((error as CustomError).name).to.equal('Length Required');
+      expect((error as CustomError).message).to.equal('Content length error');
+    }
+  });
+
+  it('Should return an error when not informing all fields', async function () {
+    const notificationRepository = new InMemoryNotificationsRepository();
+
+    const sendNotification = new SendNotification(notificationRepository);
+
+    try {
+      await sendNotification.execute({
+        recipientId: 10,
+        category: '',
+        content: 'This is a notification',
+      });
+    } catch (error) {
+      expect((error as CustomError).name).to.equal('Bad Request');
+      expect((error as CustomError).message).to.equal('Missing category or recipientId fields');
+    }
+  });
+});
