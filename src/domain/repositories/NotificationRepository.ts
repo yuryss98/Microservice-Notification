be793ad5bb4a04acd18e7079ec01ebfef81c5974
Yury Stolarz <yurysscareer@gmail.com>
2023-02-15 19:14:37 -0300

    feat: add method countManyByRecipientId in repository

--- src/domain/repositories/NotificationRepository.ts
+++ src/domain/repositories/NotificationRepository.ts
@@ -4,4 +4,5 @@ export default abstract class NotificationRepository {
   abstract create(notification: Notification): Promise<void>;
   abstract findById(notificationId: number): Promise<Notification | undefined>;
   abstract save(notification: Notification): Promise<void>;
+  abstract countManyByRecipientId(recipientId: number): Promise<number>;
 }
