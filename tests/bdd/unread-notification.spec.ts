09fa755931d4f2e8035450f17cca012a221f4501
Yury Stolarz <yurysscareer@gmail.com>
2023-02-15 19:32:27 -0300

    test: add test of unread notification

---
+++ tests/bdd/unread-notification.spec.ts
@@ -0,0 +1,38 @@
+import * as chai from 'chai';
+import { UnreadNotification } from '../../src/domain/use-cases';
+import InMemoryNotificationsRepository from '../repository/InMemoryNotificationsRepository';
+import makeNotification from '../factory/notification-factory';
+import CustomError from '../../src/domain/error/CustomError';
+
+const { expect } = chai;
+
+describe('Unread notification', function () {
+  const notificationRepository = new InMemoryNotificationsRepository();
+  const unReadNotification = new UnreadNotification(notificationRepository);
+
+  it('Should be able to mark notification as unread', async function () {
+    const recipientId = 10;
+    const newNotifiaction = makeNotification(recipientId);
+
+    await notificationRepository.create(newNotifiaction);
+
+    await unReadNotification.execute({ notificationId: newNotifiaction.id });
+
+    expect(notificationRepository.notifications[0].readAt).to.deep.equal(null);
+  });
+
+  it('Should throw an error when not finding a notification', async function () {
+    const recipientId = 10;
+    const newNotifiaction = makeNotification(recipientId);
+
+    await notificationRepository.create(newNotifiaction);
+
+    try {
+      const invalidId = 9999;
+      await unReadNotification.execute({ notificationId: invalidId });
+    } catch (error) {
+      expect((error as CustomError).name).to.equal('Not Found');
+      expect((error as CustomError).message).to.equal('Notification Not Found');
+    }
+  });
+});
