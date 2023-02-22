69c7694f6daf3ff7b7c7e4b568d365675ca612a9
Yury Stolarz <yurysscareer@gmail.com>
2023-02-22 13:58:26 -0300

    fix: error in repository parameters

--- src/domain/repositories/NotificationRepository.ts
+++ src/domain/repositories/NotificationRepository.ts
@@ -2,8 +2,8 @@ import Notification from '../entities/Notification';
 
 export default abstract class NotificationRepository {
   abstract create(notification: Notification): Promise<void>;
-  abstract findById(notificationId: number): Promise<Notification | undefined>;
+  abstract findById(notificationId: string): Promise<Notification | undefined>;
   abstract save(notification: Notification): Promise<void>;
-  abstract countManyByRecipientId(recipientId: number): Promise<number>;
-  abstract findManyByRecipientId(recipientId: number): Promise<Notification[]>;
+  abstract countManyByRecipientId(recipientId: string): Promise<number>;
+  abstract findManyByRecipientId(recipientId: string): Promise<Notification[]>;
 }
