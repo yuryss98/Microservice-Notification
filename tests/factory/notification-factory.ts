3fa2eb9e7d5073dae5c884af7c258f3408441fae
Yury Stolarz <yurysscareer@gmail.com>
2023-02-22 13:59:50 -0300

    test: fixing the notification factory

--- tests/factory/notification-factory.ts
+++ tests/factory/notification-factory.ts
@@ -1,7 +1,10 @@
 import Content from '../../src/domain/entities/Content';
 import Notification from '../../src/domain/entities/Notification';
 
-export default function makeNotification(recipientId: number, content?: string, category?: string) {
-  const newContent = new Content(content || 'Você tem uma nova solicitação de amizade');
-  return new Notification(newContent, category || 'social', recipientId);
+export default function makeNotification(recipientId: string, content?: string, category?: string) {
+  return new Notification({
+    recipientId,
+    category: category ?? 'social',
+    content: new Content(content || 'Você tem uma nova solicitação de amizade'),
+  });
 }
