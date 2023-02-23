a3179ea821788c8a8ed4613855b2f069212b58e6
Yury Stolarz <yurysscareer@gmail.com>
2023-02-23 15:14:00 -0300

    refactor: adjusting file import and export

---
+++ src/infra/factory/index.ts
@@ -0,0 +1,15 @@
+import makeCancelNotificationController from './cancel-notification-controller-factory';
+import makeCountRecipientNotificationsController from './count-notification-controller-factory';
+import makeGetRecipientNotificationsController from './get-notification-controller-factory';
+import makeReadNotificationController from './read-notification-controller-factory';
+import makeUnreadNotificationController from './unread-notification-controller-factory';
+import makeSendNotificationController from './send-notification-controller-factory';
+
+export {
+  makeCancelNotificationController,
+  makeCountRecipientNotificationsController,
+  makeGetRecipientNotificationsController,
+  makeReadNotificationController,
+  makeUnreadNotificationController,
+  makeSendNotificationController,
+};
