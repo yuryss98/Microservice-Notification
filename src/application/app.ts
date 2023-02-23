c4a260b8cb784a5f522e985d747a779c059a7fc9
Yury Stolarz <yurysscareer@gmail.com>
2023-02-23 15:18:13 -0300

    feat: add notifications routes in application

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import helmet from 'helmet';
 import errorHandler from '../infra/middleware/error-handle';
+import notificationRoutes from '../infra/routes/notification.routes';
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
 
+app.use('/notifications', notificationRoutes);
 app.use(errorHandler);
 
 export default app;
