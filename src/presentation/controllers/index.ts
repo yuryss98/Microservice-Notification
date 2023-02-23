03857c1dfe7919edb8beb9d5252bb037bb049ad1
Yury Stolarz <yurysscareer@gmail.com>
2023-02-23 15:01:52 -0300

    refactor: Adjusting file import and export

---
+++ src/presentation/controllers/index.ts
@@ -0,0 +1,15 @@
+import CancelNotificationController from './Cancel-notification-controller';
+import CountRecipientNotificationController from './Count-recipient-notifications-controller';
+import GetRecipientNotificationsController from './Get-recipient-notifications-controller';
+import ReadNotificationController from './Read-notification-controller';
+import SendNotificationController from './Send-notification-controller';
+import UnreadNotificationController from './Unread-notification-controller';
+
+export {
+  CancelNotificationController,
+  CountRecipientNotificationController,
+  GetRecipientNotificationsController,
+  ReadNotificationController,
+  SendNotificationController,
+  UnreadNotificationController,
+};
