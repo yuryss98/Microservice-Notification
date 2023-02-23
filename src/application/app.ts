c33617edfb5e32415927d806cabec6722b786b11
Yury Stolarz <yurysscareer@gmail.com>
2023-02-23 14:55:15 -0300

    feat: add error handle in application

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import helmet from 'helmet';
+import errorHandler from '../infra/middleware/error-handle';
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
+
+app.use(errorHandler);
 
 export default app;
